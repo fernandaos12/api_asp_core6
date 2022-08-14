70c43120a37cbe3f77e5a19fcaec69bbfd6dc9b8
Fernanda Oliveira <fernandaos12@gmail.com>
2022-08-14 15:56:08 -0300

    Controller Base Pokemon

--- PokeAcademyAPI/Controllers/PokemonsController.cs
+++ PokeAcademyAPI/Controllers/PokemonsController.cs
@@ -7,6 +7,40 @@ namespace PokeAcademyAPI.Controllers
     public class PokemonController : ControllerBase
     {
         private const string ALL_POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
-        private const string EXTERNAL_BASE_URL = 
+        private const string EXTERNAL_BASE_URL = "https://localhost:7235";
+        private const string TOKEN = "Bearer ";
+
+        public PokemonController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(int limit)
+        {
+            return Ok();
+        }
+
+         [HttpGet("id")]
+        public async Task<IActionResult> GetbyId(int id)
+        {
+            return Ok();
+        }
+
+         [HttpPost]
+        public async Task<IActionResult> Post(Ping ping)
+        {
+            return Ok();
+        }
+    }
+
+    public class Ping{
+        public string Message { get; set; }
+    }
+
+     public class ExternalResponse
+     {
+      public List<string> Headers {get; set;}
+      public Ping Data { get; set; }
     }
 }
